a031612a62f5c85fdc50a2a5b81f06041f34e8a5
Theo Wright <theowright2015@gmail.com>
2019-05-11 22:29:33 +0100

    first and second modal, api pull, timer, done

--- client/src/views/MainPage.vue
+++ client/src/views/MainPage.vue
@@ -8,7 +8,9 @@
       <div id="map-container">
         <MapView id="mapView" />
       </div>
-        <ModalButtons id="buttonsContainer" />
+      <div id="buttons-container">
+        <ModalButtons id="buttons" />
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +38,8 @@ export default {
     .then(response => response.json())
     .then(location => this.location = location);
 
+
+
     // fetch('http://localhost:3000/api/messages/')
     // .then(response => response.json())
     // .then(messages => this.messages = messages);
@@ -51,11 +55,7 @@ export default {
     display: flex;
     height: 100%;
     flex-wrap: row wrap;
-    /* justify-content: space-between; */
-
-    /* flex-direction: row; */
     text-align: center;
-
   }
 
   #container > * {
@@ -64,14 +64,20 @@ export default {
   border: 2px solid blue;
 }
 
-  #buttonsContainer {
+  #buttons-container {
+    border: 3px solid black;
+  }
+
+  #buttons {
     border: 2px solid blue;
+    height: 100%;
+
   }
 
   #form-container {
     display: flex;
     flex-direction: column;
-
+    justify-content: space-around;
   }
 
   #messageList {
@@ -88,10 +94,10 @@ export default {
     /* align-self: flex-start; */
   }
 
-  #mapContainer {
+  #map-container {
     display: flex;
     align-items: center;
-    justify-content: center;
+    /* justify-content: center; */
   }
 
   #mapView {
@@ -107,13 +113,13 @@ export default {
   #form-container { flex: 1 0 0; }
 }
 @media all and (min-width: 20vw) {
-#buttonsContainer { flex: 1 0 0; }
+#buttons-container { flex: 1 0 0; }
 }
 
 @media all and (min-width: 40vw) {
   #map-container    { flex: 4 0px; }
-  .aside-1 { order: 1; }
-  .main    { order: 2; }
+  .form-container { order: 1; }
+  .map-container   { order: 2; }
   .aside-2 { order: 3; }
   .footer  { order: 4; }
 }
