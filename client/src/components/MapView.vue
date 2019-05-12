a2de7e4462ec5f354794acfe46962a00da637718
S-Maitland <scott@maitland.me.uk>
2019-05-13 00:17:00 +0100

    leaflet setup initialized but hitting errors

--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -1,36 +1,36 @@
 <template lang="html">
   <div class="map">
-    <p>map view</p>
-    <div id="mapid">
-      <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint  </p>
-    </div>
+    <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution">
+      </l-tile-layer>
+    </l-map>
   </div>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
 
 export default {
   name: 'issMap',
-  data() {
-    return {
-      zoom:13,
-      center: L.latLng(47.413220, -1.219482),
-      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(47.413220, -1.219482),
-    }
-  },
   components: {
     LMap,
     LTileLayer,
     LMarker
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: L.latLng(47.413220, -1.219482),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org.copyright">Open Street Map</a> contributors',
+      marker: L.latLng(47.413220, -1.219482)
+    }
   }
 }
 </script>
 
 <style lang="css" scoped>
-#map {
-  height: 180px;
+.map {
+  height: 95vh;
 }
 </style>
