664f78cf0c6c825b310e1eb3a6954acdbfc9fd16
dataSlayer64275 <ess.knst@gmail.com>
2019-05-12 14:28:53 +0100

    Adds countries API

--- client/src/components/InputForm.vue
+++ client/src/components/InputForm.vue
@@ -1,11 +1,22 @@
 <template lang="html">
   <div class="">
-    <p>input form</p>
+    <p>Input Form</p>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'input-form',
+  data () {
+    return {
+    countries:[]
+    }
+  },
+  mounted(){
+  fetch("https://restcountries.eu/rest/v2/all")
+  .then(res => res.json())
+  .then(countries => this.countries = countries)
+  }
 }
 </script>
 
