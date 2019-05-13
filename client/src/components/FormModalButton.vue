945c1c3d386c5cfc27c90f682b05277f4dc9bac4
dataSlayer64275 <ess.knst@gmail.com>
2019-05-13 13:55:58 +0100

    Added modal functionality for input form

---
+++ client/src/components/FormModalButton.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div>
+    <InputForm v-if="isModalVisible"/>
+    <button type="button" class="modal-btn" @click="showForm">
+      Send a message!
+    </button>
+    <modal v-show="isModalVisible" name="formModal"/>
+    </div>
+</template>
+
+<script>
+
+import {eventBus } from '@/main.js';
+import InputForm from '@/components/InputForm';
+
+export default {
+  components: {
+    InputForm
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showForm() {
+      this.isModalVisible = true;
+    }
+  },
+  mounted() {
+    eventBus.$on('close', () => {
+      this.isModalVisible = false
+    })
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
