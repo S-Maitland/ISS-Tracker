a23899db7c71fc07c94239379a6e60a1c9954d3a
S-Maitland <scott@maitland.me.uk>
2019-05-14 17:05:10 +0100

    update to resolve Ib error on map rendering

--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -14,17 +14,24 @@ export default {
     LTileLayer,
     LMarker
   },
+  data: function(){
+    return {
+      earth: null,
+      marker: null,
+      marker2: null,
+    }
+  },
   methods: {
     initialize() {
-      earth: new WebGLEarth('earth_div');
+      this.earth = new WebGLEarth('earth_div');
 
       WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.earth);
 
-      marker: WE.marker([this.location.iss_position.latitude,this.location.iss_position.longitudes]).addTo(this.earth);
-      marker.bindPopup("<b>ISS</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+      this.marker = WE.marker([this.location.iss_position.latitude,this.location.iss_position.longitudes]).addTo(this.earth);
+      this.marker.bindPopup("<b>ISS</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
 
-      marker2: WE.marker([30.058056, 31.228889]).addTo(this.earth);
-      marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+      this.marker2 = WE.marker([30.058056, 31.228889]).addTo(this.earth);
+      this.marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
 
       this.earth.setView([51.505, 0], 6);
     }
