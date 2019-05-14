188ffe9b108d939c5ca5512c1f853ebb149487dc
S-Maitland <scott@maitland.me.uk>
2019-05-14 14:32:17 +0100

    WebGLEarth overlay implemented

--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="map">
+  <div class="earth">
     <l-map
     :zoom="zoom"
     :center="center"
@@ -28,23 +28,45 @@ export default {
     LTileLayer,
     LMarker
   },
-  data() {
-    return {
-      zoom: 3,
-      center: L.latLng(this.location.iss_position.latitude,this.location.iss_position.longitude),
-      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      attribution:
-      '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      marker:L.latLng(this.location.iss_position.latitude,this.location.iss_position.longitude)
+  // data() {
+  //   return {
+  //     zoom: 3,
+  //     center: L.latLng(this.location.iss_position.latitude,this.location.iss_position.longitude),
+  //     url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+  //     attribution:
+  //     'NASA',
+  //     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  //     marker:L.latLng(this.location.iss_position.latitude,this.location.iss_position.longitude)
+  //   }
+  // }
+  methods: {
+      initialize() {
+        earth: new WebGLEarth('earth_div');
+        WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+
+        marker: WE.marker([51.5, -0.09]).addTo(earth);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+
+        marker2: WE.marker([30.058056, 31.228889]).addTo(earth);
+        marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+
+        markerCustom: WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+        earth.setView([51.505, 0], 6);
     }
+  },
+  mounted(){
+    this.initialize();
   }
 }
 </script>
 
 <style lang="css" scoped>
-.map {
-  height: 100%;
-  width: 100%;
-}
+/* .map {
+height: 100%;
+width: 100%;
+} */
+
+html, body{padding: 0; margin: 0; background-color: black;}
+#earth_div{top: 0; right: 0; bottom: 0; left: 0; position: absolute !important;}
 </style>
